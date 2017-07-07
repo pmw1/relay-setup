f6a8461a2f48718856d1c146499f52e160d58a09
Ubuntu <ubuntu@ip-172-31-13-144.ec2.internal>
2017-07-07 06:38:06 +0000

    second commit

---
+++ basic-init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+clear
+echo "--------------------------------------------------------------------"
+echo "------------------  Starting Relay Setup Init  ---------------------"
+echo "--------------------------------------------------------------------"
+
+sleep 1
+
+sudo apt-get update
+sudo apt-get install -y nload htop git build-essential 
+
+cd ~
+git clone git://git.videolan.org/bitstream.git
+cd bitstream
+make install
+
+cd ~
+mkdir apps
+cd ~/apps
+git clone https://github.com/mmalecki/multicat.git
+
+cd ~/apps/multicat/trunk
+pwd
+sudo make
+sudo make install
